177b3de1f0dadb755c33d7f76108ab90c3c1af84
Ripudaman Singh <rdsingh120@gmail.com>
2025-09-27 17:17:47 +0530

    create flight booking platform project

---
+++ src/main/java/com/singh/ripudaman/FlightBookingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.singh.ripudaman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singh/ripudaman/FlightBookingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singh.ripudaman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
